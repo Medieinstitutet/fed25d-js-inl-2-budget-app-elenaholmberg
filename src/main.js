45ee2ae5085b3219f70f3d8c1ee8551ce41dc07b
elena holmberg <elenaholmberg83@hotmail.com>
2026-02-03 15:57:28 +0100

    💫 JS updated

--- src/main.js
+++ src/main.js
@@ -1,28 +1,26 @@
 import "./style.css";
 
-//Hämtar alla mina element från html//
 // Hämta elementen från HTML
 const incomeInput = document.getElementById("incomeSum");
 const addBtn = document.getElementById("addIncomeBtn");
 const resultInput = document.getElementById("calculateIncome");
 
 // När man klickar på knappen
-let total = 0;
+
+let totalIncome = 0; //värdet ska nollas efter varje gång
 
 addBtn.addEventListener("click", function () {
   const value = Number(incomeInput.value);
+  /*input-fält är alltid sträng så jag måste göra om till Number. 
+  Const value =... betyder att vi sparar om siffran i en variabel. Variabeln döper jag till value.*/
 
-  if (!value) {
-    alert("Skriv in ett tal!");
-    return;
-  }
-
-  total += value;
-  resultInput.value = total;
+  totalIncome += value;
+  resultInput.value = totalIncome;
   incomeInput.value = "";
 });
 
+const expenseInput = document.getElementById("#expenseSum");
+const addBtn2 = document.getElementById("#addExpenseBtn");
+const resultInput2 = document.getElementById("calculateExpense");
 
-/*
-[] 2. Uppgift: lägg till ifylld siffra från input-fält Summa  (id="expenseSum") genom att trycka på "Lägg till-knappen
-("id="addExpenseBtn"). Siffran ska då dyka upp i input-fält "Utgående:" (id="calculateExpense")
+let totalExpense = 0;
