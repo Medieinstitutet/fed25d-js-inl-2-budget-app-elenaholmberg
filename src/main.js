37084b55fd4fb83f436599a4c9372a4329832910
elena holmberg <elenaholmberg83@hotmail.com>
2026-02-03 13:32:35 +0100

    checked conection between JS and HTML

---
+++ src/main.js
@@ -0,0 +1,14 @@
+import "./style.css";
+
+orderForm(); // startar formulärlogiken
+// Tom array som ska innehålla varukorgen
+
+/*const incomeSum = [
+  {
+    
+  }
+] */ const addIncomeBtn =
+  document.querySelector(
+    "#incomeSum",
+  ); /*Kopplar oss till olika platser i HTML-dokumentet.
+ querySelector hämtar element med angivet id.*/
