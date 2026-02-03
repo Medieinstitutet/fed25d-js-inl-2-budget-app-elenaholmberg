2e42b4ea38aefcb223bf7f39c8f1902573ff8ce6
elena holmberg <elenaholmberg83@hotmail.com>
2026-02-03 16:15:47 +0100

    💫 JS updated

--- src/main.js
+++ src/main.js
@@ -1,9 +1,13 @@
 import "./style.css";
 
-// Hämta elementen från HTML
-const incomeInput = document.getElementById("incomeSum");
-const addBtn = document.getElementById("addIncomeBtn");
-const resultInput = document.getElementById("calculateIncome");
+///----------------------------------------///
+///-------------------IN-------------------///
+///-----------------------------------------///
+
+// Hämta elementen från HTML  //varför döper man inte variabeln till samma som tillhörande id?
+const incomeInput = document.getElementById("incomeSum"); //här skriver användaren sitt tal
+const addBtn = document.getElementById("addIncomeBtn"); //Lägg till-knappen
+const resultInput = document.getElementById("calculateIncome"); //Hit flyttas talet
 
 // När man klickar på knappen
 
@@ -12,12 +16,18 @@ let totalIncome = 0; //värdet ska nollas efter varje gång
 addBtn.addEventListener("click", function () {
   const value = Number(incomeInput.value);
   /*input-fält är alltid sträng så jag måste göra om till Number. 
-  Const value =... betyder att vi sparar om siffran i en variabel. Variabeln döper jag till value.*/
+  Const value =... betyder att vi sparar om siffran i en variabel. Variabeln döper jag till value.
+  dvs value är samma som incomeInput som i html heter #incomeSum (=>#incomeSum =>incomeInput =>value */
 
-  totalIncome += value;
+  totalIncome = totalIncome + value; //förstår inte helt vad denna rad är, gör eller kommer ifrån?
+
   resultInput.value = totalIncome;
   incomeInput.value = "";
 });
+
+///----------------------------------------///
+///-------------------UT-------------------///
+///-----------------------------------------///
 
 const expenseInput = document.getElementById("#expenseSum");
 const addBtn2 = document.getElementById("#addExpenseBtn");
