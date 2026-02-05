3e627500a19eac4dd5da467baed66fd97403a612
elena holmberg <elenaholmberg83@hotmail.com>
2026-02-05 13:55:03 +0100

    ⚰️ Remove dead code

--- src/main.ts
+++ src/main.ts
@@ -42,39 +42,3 @@ expenseBtn?.addEventListener("click", () => {
   dropdownExp.disabled = false; // aktivera utgiftsdropdown
   dropdownIn.disabled = true; // stäng inkomstdropdown
 });
-
-///---------------------------------------///
-///--------------UTGIFT------------------///
-///---------------------------------------///
-
-//filtrering
-const expensesDropdown = document.querySelector("#expensesDropdown");
-if (expensesDropdown) {
-  categories.expenses.forEach((category) => {
-    expensesDropdown.innerHTML += `<option value="${category.value}">${category.text}</option>`;
-  });
-}
-
-///----------------------------------------///
-///-------------------IN-------------------///
-///-----------------------------------------///
-
-const incoming = document.getElementById("incoming"); //här skriver användaren sitt tal
-const chooseIncomeBtn = document.getElementById("chooseIncomeBtn"); //Inkomst radio-button
-const addBtn = document.getElementById("addBtn"); //knappen flyttar ner summan i nästa fieldset
-const incomeInput = document.getElementById("incomeInput");
-///--- När man klickar på radiobutton----///
-
-let totalIncome = 0;
-
-addBtn.addEventListener("click", function () {
-  const value = Number(incoming.value);
-  /*input-fält är alltid sträng, så jag måste göra om till Number. 
-  Const value =... betyder att vi sparar om siffran i en variabel. Variabeln döper jag till value.
-  dvs value är värdet "incoming" fast i nummer-format.*/
-
-  if (!chooseIncomeBtn.checked) return;
-  if (isNaN(value) || value === 0) return;
-
-  totalIncome = totalIncome + value; //    betyder: ta det som redan finns i totalIncome och lägg till det nya värdet
-});
