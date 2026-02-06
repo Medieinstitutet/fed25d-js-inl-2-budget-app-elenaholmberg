71fbc4c4cb4f0f0bbb25e5856864d8fad652ef81
elena holmberg <elenaholmberg83@hotmail.com>
2026-02-06 10:04:15 +0100

    ✅  both dropdowns

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,8 @@ import "./style.css";
 import categories from "./categories.json";
 
 ///---------------------------------------///
-///--------------INKOMST------------------///
+///--------------FILTRERING---------------///
 ///---------------------------------------///
-
-//filtrering
 const incomeDropdown = document.querySelector("#incomeDropdown");
 if (incomeDropdown) {
   categories.incomes.forEach((category) => {
@@ -14,31 +12,33 @@ if (incomeDropdown) {
   });
 }
 
-//vid klick av radiobutton
-const incomeBtn = document.querySelector<HTMLInputElement>("#chooseIncomeBtn");
-const dropdownIn = document.querySelector<HTMLSelectElement>("#incomeDropdown");
+const expensesDropdown = document.querySelector("#expensesDropdown");
+if (expensesDropdown) {
+  categories.expenses.forEach((category) => {
+    expensesDropdown.innerHTML += `<option value="${category.value}">${category.text}</option>`;
+  });
+}
 
-const expenseBtn =
-  document.querySelector<HTMLInputElement>("#chooseExpenseBtn");
-const dropdownExp =
-  document.querySelector<HTMLSelectElement>("#expensesDropdown");
+///----------------------------------------///
+///-------------------IN-------------------///
+///-----------------------------------------///
 
-// Alla dropdowns disabled från början
-if (dropdownIn) dropdownIn.disabled = true;
-if (dropdownExp) dropdownExp.disabled = true;
+const incoming = document.getElementById("incoming"); //här skriver användaren sitt tal
+const chooseIncomeBtn = document.getElementById("chooseIncomeBtn"); //Inkomst radio-button
+const addBtn = document.getElementById("addBtn"); //knappen flyttar ner summan i nästa fieldset
+const incomeInput = document.getElementById("incomeInput");
+///--- När man klickar på radiobutton----///
 
-// När man klickar på inkomst button => dropdown aktiveras
-incomeBtn?.addEventListener("click", () => {
-  if (!dropdownIn || !dropdownExp) return;
+let totalIncome = 0;
 
-  dropdownIn.disabled = false; // aktivera inkomstdropdown
-  dropdownExp.disabled = true; // stäng utgiftsdropdown
-});
+addBtn.addEventListener("click", function () {
+  const value = Number(incoming.value);
+  /*input-fält är alltid sträng, så jag måste göra om till Number. 
+  Const value =... betyder att vi sparar om siffran i en variabel. Variabeln döper jag till value.
+  dvs value är värdet "incoming" fast i nummer-format.*/
 
-// När man klickar på utgifts button => dropdown aktiveras
-expenseBtn?.addEventListener("click", () => {
-  if (!dropdownIn || !dropdownExp) return;
+  if (!chooseIncomeBtn.checked) return;
+  if (isNaN(value) || value === 0) return;
 
-  dropdownExp.disabled = false; // aktivera utgiftsdropdown
-  dropdownIn.disabled = true; // stäng inkomstdropdown
+  totalIncome = totalIncome + value; //    betyder: ta det som redan finns i totalIncome och lägg till det nya värdet
 });
