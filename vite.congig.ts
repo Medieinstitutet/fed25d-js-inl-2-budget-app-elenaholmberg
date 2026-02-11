64692387aa8c875315dd84dcce8f498d9f28bf6e
elena holmberg <elenaholmberg83@hotmail.com>
2026-02-11 20:45:02 +0100

    wtf

--- vite.congig.ts
+++ vite.congig.ts
@@ -1,5 +1,7 @@
 import { defineConfig } from "vite";
 
-export default defineConfig({
+//github.com/Medieinstitutet/fed25d-js-intro-inl-1-elenaholmberg/blob/main/vite.config.js
+
+https: export default defineConfig({
   base: "/fed25d-js-inl-2-budget-app-elenaholmberg/",
 });
